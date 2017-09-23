76acd127f33ebeaeba2a0b9f5391c547f13065fd
AmrBekhiet <t-ambekh@microsoft.com>
2017-09-23 17:12:28 +0200

    Inital Demo

---
+++ PotatoQuiz/PotatoQuiz/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using GeekQuiz.Models;
+using System.Data.Entity;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace PotatoQuiz
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer<TriviaDbContext>(new DataInitializer());
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
